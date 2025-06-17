6abdbe8f07f001c5a630c229ee746bea8e82d489
devendra138 <rawool.devendra138@gmail.com>
2025-06-17 18:59:07 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OfficeSuit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
