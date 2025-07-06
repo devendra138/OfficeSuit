74b7a0b2e29c61f277b21aae1649c45e490bbdeb
devendra138 <rawool.devendra138@gmail.com>
2025-07-06 11:18:22 +0530

    Login model

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,5 +17,27 @@ namespace OfficeSuit.Controllers
         {
             return View();
         }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LoginUser(string username, string password)
+        {
+            if (username == "Darshan" && password == "Darshan@123")
+            {
+                TempData["Info"] = "Login Successful.";
+                return RedirectToAction("Login");
+            }
+            else
+            {
+                TempData["Info"] = "Wrong username or password. Please try again.";
+                return RedirectToAction("Login");
+            }
+        }
+
+
     }
 }
