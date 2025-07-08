e0b35c616cbd0c97398691915ffff188348b1779
darsh <darsh@LAPTOP-AEMNNG68>
2025-07-08 22:37:16 +0530

    commit stage

---
+++ Models/UserInfo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OfficeSuit.Models
+{
+    public class UserInfo
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        [StringLength(50, ErrorMessage = "First Name can't be longer than 50 characters")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        [StringLength(50, ErrorMessage = "Last Name can't be longer than 50 characters")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Contact is required")]
+        [Phone(ErrorMessage = "Invalid phone number")]
+        public string Contact { get; set; }
+
+        [Required(ErrorMessage = "Gender is required")]
+        public string Gender { get; set; } // Could also be an enum
+
+        [Required(ErrorMessage = "Designation is required")]
+        [StringLength(100)]
+        public string Designation { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
