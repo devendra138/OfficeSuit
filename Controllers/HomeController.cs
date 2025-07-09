ee354f3d8bd21bb84639d1f28b144ae44c6b7714
devendra138 <rawool.devendra138@gmail.com>
2025-07-09 07:27:34 +0530

    insert registration data

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,17 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using OfficeSuit.Models;
+using System.Data;
 
 namespace OfficeSuit.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+
+        private readonly string connectionString = "Server=DESKTOP-IDJHK8U;Database=OfficeSuit;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
+
+
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -43,10 +48,45 @@ namespace OfficeSuit.Controllers
             return View();
         }
 
-        public IActionResult UserRegistration(UserInfo userInfo)
+        public IActionResult UserRegistration(UserInfo user)
         {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("InsertUserInfo", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
 
-            return View();
+                    cmd.Parameters.AddWithValue("@FirstName", user.FirstName);
+                    cmd.Parameters.AddWithValue("@LastName", user.LastName);
+                    cmd.Parameters.AddWithValue("@Email", user.Email);
+                    cmd.Parameters.AddWithValue("@Password", user.Password);
+                    cmd.Parameters.AddWithValue("@Contact", user.Contact);
+                    cmd.Parameters.AddWithValue("@Gender", user.Gender);
+                    cmd.Parameters.AddWithValue("@Designation", user.Designation);
+
+                    try
+                    {
+                        conn.Open();
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            TempData["Info"] = "Registration successful.";
+                            return RedirectToAction("Registration");
+                        }
+                        else
+                        {
+                            TempData["Info"] = "Registration failed. Please try again.";
+                            return RedirectToAction("Registration");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        TempData["Info"] = "An error occurred during registration. Please try again.";
+                        // Log error (ex)
+                        return RedirectToAction("Registration");
+                    }
+                }
+            }
         }
     }
 }
